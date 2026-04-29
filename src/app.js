3e11bb4c531637ded8f3d555d612e6d7fcdcb49b
Rahul Kumar <kumar.coding.practice@gmail.com>
2026-04-29 21:36:12 +0530

    Added winston and morgan logger for logging infos

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import logger from "./utils/logger.js";
+import morgan from "morgan";
+
+const morganFormat = ":method :url :status :response-time ms";
 
 const app = express();
 
@@ -22,6 +26,22 @@ app.use(
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
   }),
 );
 
